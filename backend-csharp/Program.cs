6fa5b1d6e9bae87b5fcff6ea19bb869103ea6457
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2026-03-05 20:15:39 -0300

    feat(csharp): implement phase 0 contract and top-level structure

--- backend-csharp/Program.cs
+++ backend-csharp/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello AeroStack!");
+
+app.MapGet("/decolamos", () => "Decolamos!");
+
+app.MapGet("/aircraft", () => new List<AircraftV1>());
+
 
 app.Run();
+
+record AircraftV1(Guid Id, string Model, string Manufacturer, int Year);
+
+record AircraftV2
+{
+    public required Guid Id { get; init; }
+    public required string Model { get; init; }
+    public required string Manufacturer { get; init; }
+    public required int Year { get; init; }
+}
+
+
