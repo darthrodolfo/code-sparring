09ef26bf5af88e1fef4059f694cbd4b2ca65302d
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2026-03-12 02:42:28 -0300

    Revert "feat(node-nestjs): migrate NestJS runtime from Express to Fastify"
    This reverts commit b4dd100e59effc543deb63dbcf8c14767c0465fe.

--- backend-node-nest-js/src/main.ts
+++ backend-node-nest-js/src/main.ts
@@ -1,15 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
