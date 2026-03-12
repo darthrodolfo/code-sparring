106d45020f9ea791e8495586514ecc3a4ff5c2eb
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2026-03-11 22:53:36 -0300

    feat (node-fsfy): define aircraft domain types and interfaces
    - Introduces enums for aircraft roles and statuses to ensure type safety.
    - Implements the `AircraftV2` interface to support detailed specifications, geolocation, and historical data tracking.

---
+++ backend-node-fastify/src/routes/aircraft/aircraft.types.ts
@@ -0,0 +1,55 @@
+export enum AircraftRole {
+  Fighter = "Fighter",
+  Bomber = "Bomber",
+  Transport = "Transport",
+  Reconnaissance = "Reconnaissance",
+  Drone = "Drone",
+  Trainer = "Trainer",
+}
+
+export enum AircraftStatus {
+  Active = "Active",
+  Retired = "Retired",
+  Maintenance = "Maintenance",
+  Stored = "Stored",
+}
+
+export interface Geolocation {
+  latitude: number;
+  longitude: number;
+}
+export interface AircraftSpecs {
+  maxSpeedKmh: number; // in km/h
+  wingspanMeters: number; // in km
+  rangeKm: number; // in meters
+  maxAltitudeMeters: number | null;
+  flightEndurance: string; // ISO 8601 duration ex: "PT14H30M"
+}
+
+export interface ConflictHistory {
+  conflictName: string;
+  startDate: string; // ISO 8601 date string
+  endDate: string; // ISO 8601 date string
+  roleInConflict: AircraftRole;
+}
+
+export interface AircraftV2 {
+  id: string;
+  model: string;
+  manufacturer: string;
+  serialNumber: string;
+  yearOfManufacture: string;
+  priceMillionUSD: string; // decimal serializado como string
+  emptyWeightKg: number;
+  status: AircraftStatus;
+  firstFlightDate: string; // ISO 8601 date: "YYYY-MM-DD"
+  lastMaintenanceTime: string; // ISO 8601 datetime: "2024-01-01T00:00:00Z"
+  baseLocation: Geolocation;
+  specs: AircraftSpecs;
+  conflictHistory: ConflictHistory[];
+  metadata: Record<string, string>;
+  estimatedUnitsProduced: number | null;
+  estimatedActivateUnits: number | null;
+  photoUrl: string | null;
+  manualArchive: string | null; //base64 encoded document
+}
